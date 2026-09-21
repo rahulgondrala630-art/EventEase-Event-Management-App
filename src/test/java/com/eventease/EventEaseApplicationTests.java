1ad66083f67703073ae948fdefe0af7e1cc52838
Rahul Gondrala <rahulgondrala630@gmail.com>
2026-09-21 18:50:43 -0500

    Add EventEase test foundation

---
+++ src/test/java/com/eventease/EventEaseApplicationTests.java
@@ -0,0 +1,8 @@
+package com.eventease;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class EventEaseApplicationTests {
+    @Test void starterTestIsWired() { assertTrue(true); }
+}
